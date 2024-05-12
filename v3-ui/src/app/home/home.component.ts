28a983f35a5a1a45eb90a6d2a0c732d2b50bf076
IsaacIngram <me@isaacingram.dev>
2024-05-11 19:41:13 -0500

    Add about component and monospace font

--- v3-ui/src/app/home/home.component.ts
+++ v3-ui/src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { AboutComponent} from "../about/about.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   standalone: true,
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  imports: [AboutComponent]
 })
 export class HomeComponent {
 
