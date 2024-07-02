616e806763fed956253a2f276e15e39ca3657ebe
IsaacIngram <me@isaacingram.dev>
2024-07-01 20:05:55 -0500

    Add social icons at bottom of home component

--- v3-ui/src/app/home/home.component.ts
+++ v3-ui/src/app/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { AboutComponent} from "../about/about.component";
 import { ProjectsComponent } from "../projects/projects.component";
+import {SocialIconsComponent} from "../social-icons/social-icons.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   standalone: true,
   styleUrl: './home.component.css',
-  imports: [AboutComponent, ProjectsComponent]
+  imports: [AboutComponent, ProjectsComponent, SocialIconsComponent]
 })
 export class HomeComponent {
 
