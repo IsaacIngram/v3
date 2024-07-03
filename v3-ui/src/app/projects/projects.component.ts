682f0af51fd08467480cf2eaaebd3f274e6ed56b
IsaacIngram <me@isaacingram.dev>
2024-07-02 22:21:30 -0500

    Add new projects

--- v3-ui/src/app/projects/projects.component.ts
+++ v3-ui/src/app/projects/projects.component.ts
@@ -31,21 +31,43 @@ export class ProjectsComponent {
   }
 
   getProjects(): void {
-    const mockProject1: Project = {
-      id: 1,
+    // Populate all projects. Ideally this will be done by a JSON file or
+    // by making HTTP requests in the future.
+
+    const project0: Project = {
+      id: 0,
       name: "Bits N Bytes",
       thumbnail: "test",
       description: "A new type of vending machine that utilizes computer vision and load cells to make decisions.",
     };
-    this.all_projects.push(mockProject1)
-    this.featured_projects.push(mockProject1)
-    const mockProject2: Project = {
-      id: 2,
+    this.all_projects.push(project0)
+    this.featured_projects.push(project0)
+
+    const project1: Project = {
+      id: 1,
+      name: "Summer Orienteering (Lab for CSCI-331: Intro to AI)",
+      thumbnail: "test",
+      description: "A path finding tool for 3D maps with variable speeds."
+    }
+    this.all_projects.push(project1)
+
+    const project20: Project = {
+      id: 20,
       name: "Sprout Chaperone",
       thumbnail: "test",
       description: "An electronic moisture monitor for plants.",
     };
-    this.all_projects.push(mockProject2)
+    this.all_projects.push(project20)
+
+    const project21: Project = {
+      id: 21,
+      name: "Rapid React Robot",
+      thumbnail: "test",
+      description: "FIRST Robotics Competition robot equipped with many sensors to enable automated targeting, localization, and safety features.",
+    };
+    this.all_projects.push(project21)
+
+    // Display featured projects
     this.displayed_projects = this.featured_projects
   }
 
