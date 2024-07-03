8a7e7a91a8b4dcaa5127985ad75eb9b9a64d4a21
IsaacIngram <me@isaacingram.dev>
2024-07-03 18:59:21 -0500

    Implement project detail pages

--- v3-ui/src/app/project-detail/project-detail.component.ts
+++ v3-ui/src/app/project-detail/project-detail.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {MarkdownComponent} from "ngx-markdown";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-project-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    MarkdownComponent,
+    NgIf
+  ],
   templateUrl: './project-detail.component.html',
   styleUrl: './project-detail.component.css'
 })
-export class ProjectDetailComponent {
+export class ProjectDetailComponent implements OnInit {
+  content: string | undefined = undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.loadProjectContent();
+  }
+
+  loadProjectContent(): void {
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
+    this.http.get(`assets/projects/project${id}.md`, {responseType: 'text'})
+      .subscribe(data => {
+        this.content = data;
+      })
+  }
 
 }
