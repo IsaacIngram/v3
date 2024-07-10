1b684335de380bda5138d8d04df11e8a0060ffbe
IsaacIngram <me@isaacingram.dev>
2024-07-09 21:08:27 -0500

    Route resume button to resume new page

--- v3-ui/src/app/social-icons/social-icons.component.ts
+++ v3-ui/src/app/social-icons/social-icons.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-social-icons',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './social-icons.component.html',
   styleUrl: './social-icons.component.css'
 })
