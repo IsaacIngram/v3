1bfb16d9cf95d7eba137d64bc625168719c73133
IsaacIngram <me@isaacingram.dev>
2024-09-23 22:25:18 -0400

    Add landing page

--- v3-ui/src/app/app.routes.ts
+++ v3-ui/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {ProjectDetailComponent} from "./project-detail/project-detail.component";
 import {ResumeComponent} from "./resume/resume.component";
+import {LandingComponent} from "./landing/landing.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'project/:id', component: ProjectDetailComponent },
-  { path: 'resume', component: ResumeComponent, data: { hideFooter: true }}
+  { path: 'resume', component: ResumeComponent, data: { hideFooter: true }},
+  { path: 'landing', component: LandingComponent }
 ];
